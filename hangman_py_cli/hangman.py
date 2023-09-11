71289b7775709a57f17756ddcdfec4757f48dc4e
Andrei Vidal <andrei.godoi92@gmail.com>
2023-09-11 00:24:17 -0300

    :recycle: refactor: Separating Play function from Main module

--- hangman_py_cli/hangman.py
+++ hangman_py_cli/hangman.py
@@ -1,6 +1,10 @@
 from utils import random_word_category
 from utils import show_choice_menu
+from utils import play
+
 
 if __name__ == "__main__":
-    show_choice_menu = show_choice_menu()
-    random_word_category(chosen_category=show_choice_menu)
+    chosen_category = show_choice_menu()
+    selected_word = random_word_category(chosen_category=chosen_category)
+
+    play(selected_word)
