cecc18b3536c32108f94daa7a4dfb012589e79e8
Andrei Vidal <andrei.godoi92@gmail.com>
2023-10-13 08:59:25 -0300

    Add exercise4.c

---
+++ LearningC/exercises/exercise4.c
@@ -0,0 +1,26 @@
+/*
+Escreva um programa que receba dois números, calcule e apresente o resultado
+do primeiro número elevado ao segundo.
+*/
+
+#include <stdio.h>
+#include <math.h>
+#include <locale.h>
+
+
+int main()
+{
+    setlocale(LC_ALL,"#UTF-8");
+    int number1, number2, total;
+
+    printf("Calcular potência entre dois numeros\n");
+    printf("Digite um número inteiro: ");
+    scanf("%d", &number1);
+    printf("\nDigite a potência: ");
+    scanf("%d", &number2);
+
+    total = pow(number1, number2);
+
+    printf("%d elevado a %.d é igual a %d\n\n", number1, number2, total);
+    return 0;
+}
