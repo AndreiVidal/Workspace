01e406d221a12e8db77c4a6ecd2b60b2b634cb36
AndreiVidal <andrei.godoi92@gmail.com>
2023-11-27 01:39:27 -0300

    add exercise5

---
+++ LearningC/exercises/exercise5.c
@@ -0,0 +1,29 @@
+/*
+Construa um programa que receba a idade de uma pessoa e identifique sua classe
+eleitoral: não eleitor (menor que 16 anos de idade), eleitor obrigatório (entre 18 e
+65 anos) e eleitor facultativo (entre 16 e 18 anos e maior que 65 anos).
+*/
+
+#include <stdio.h>
+#include <locale.h>
+
+void main()
+{
+    setlocale(LC_ALL, "");
+    int idade;
+
+    printf("Digite sua idade: ");
+    scanf("%d", &idade);
+    if (idade < 16)
+    {
+        printf("Você não tem idade para votar, volte dentro de %d anos \n", (16 - idade));
+    }
+    else if (idade <= 17 || idade > 65)
+    {
+        printf("Seu voto é facultattivo.\n");
+    }
+    else
+    {
+        printf("Seu voto é obrigatório.\n");
+    }
+}
