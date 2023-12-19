3ba671f351ada8a542a568c9e6c28c8998bbc69e
AndreiVidal <andrei.godoi92@gmail.com>
2023-12-19 02:57:50 -0300

    ✨ feat:
    add exercise1.py

---
+++ LearningPython/exercises/exercise1.py
@@ -0,0 +1,9 @@
+""" 
+    Escreva um programa que leia um número inteiro e apresente seu antecessor e seu sucessor. 
+"""
+
+number = int(input("Digite um número inteiro: "))
+
+print(f"o número digitado foi {number}")
+print(f"Seu antecessor é {(number -1)} ")
+print(f"Seu sucessor é {(number +1)} ")
