5a97ec44b315e35c896333535920c68dc959c0e2
AndreiVidal <andrei.godoi92@gmail.com>
2023-12-19 03:05:42 -0300

    ✨ feat:
    new exercise2.py

---
+++ LearningPython/exercises/exercise2.py
@@ -0,0 +1,10 @@
+"""
+     Elabore um programa que receba quatro notas e calcule a média aritmética entre elas.
+"""
+grade1 = float(input("Informe a nota 1: "))
+grade2 = float(input("Informe a nota 2: "))
+grade3 = float(input("Informe a nota 3: "))
+grade4 = float(input("Informe a nota 4: "))
+avarage = (grade1 + grade2 + grade3 + grade4) / 4
+
+print(f"Sua média aritmética é {avarage}")
